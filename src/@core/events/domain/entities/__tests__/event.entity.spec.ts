23c14cf0234685664af105ebd2bb3ace7963a5a1
lueberGandra <lueber_emp@hotmail.com>
2024-04-24 22:14:06 -0300

    add partner repository

--- src/@core/events/domain/entities/__tests__/event.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/event.entity.spec.ts
@@ -1,32 +1,57 @@
 import { Event } from '../event.entity';
 import { PartnerId } from '../partner.entity';
 
-test('Deve criar um evento', () => {
+test('deve criar um evento', () => {
   const event = Event.create({
-    name: 'Evento 1',
-    description: 'Descrição de evento 1',
+    name: 'Evento de teste',
+    description: 'Descrição do evento de teste',
     date: new Date(),
     partner_id: new PartnerId(),
   });
 
   event.addSection({
-    name: 'Sessão 1',
-    description: 'Descrição da sessão 1',
-    total_spots: 100,
+    name: 'Seção de teste',
+    description: 'Descrição da seção de teste',
     price: 1000,
+    total_spots: 100,
+  });
+
+  expect(event.sections.size).toBe(1);
+  expect(event.total_spots).toBe(100);
+  const [section] = event.sections;
+  expect(section.spots.size).toBe(100);
+});
+
+test('deve publicar todos os itens do evento', () => {
+  const event = Event.create({
+    name: 'Evento de teste',
+    description: 'Descrição do evento de teste',
+    date: new Date(),
+    partner_id: new PartnerId(),
   });
 
   event.addSection({
-    name: 'Sessão 2',
-    description: 'Descrição da sessão 2',
-    total_spots: 100,
+    name: 'Seção de teste',
+    description: 'Descrição da seção de teste',
     price: 1000,
+    total_spots: 100,
   });
-  console.log(event);
-  expect(event.sections.size).toBe(2);
-  expect(event.total_spots).toBe(200);
 
-  const [section] = event.sections;
+  event.addSection({
+    name: 'Seção de teste 2',
+    description: 'Descrição da seção de teste 2',
+    price: 50,
+    total_spots: 1000,
+  });
 
-  expect(section.spots.size).toBe(100);
+  event.publishAll();
+  expect(event.is_published).toBe(true);
+
+  const [section1, section2] = event.sections.values();
+  expect(section1.is_published).toBe(true);
+  expect(section2.is_published).toBe(true);
+
+  [...section1.spots, ...section2.spots].forEach((spot) => {
+    expect(spot.is_published).toBe(true);
+  });
 });
--- src/@core/events/domain/entities/partner.entity.ts
+++ src/@core/events/domain/entities/partner.entity.ts
@@ -1,7 +1,14 @@
-import { AggregateRoot } from 'src/@core/common/domain/aggregate-root';
 import { Uuid } from 'src/@core/common/domain/value-objects/uuid.vo';
+import { AggregateRoot } from '../../../common/domain/aggregate-root';
+import { Event } from './event.entity';
 
 export class PartnerId extends Uuid {}
+
+export type InitEventCommand = {
+  name: string;
+  description: string;
+  date: Date;
+};
 
 export type PartnerConstructorProps = {
   id?: PartnerId | string;
@@ -12,15 +19,33 @@ export class Partner extends AggregateRoot {
   id: PartnerId;
   name: string;
 
-  constructor(props: PartnerConstructorProps) {
+  constructor(props: PartnerConstructorProps, id?: PartnerId) {
     super();
-    this.id = typeof props.id === 'string' ? new PartnerId(props.id) : props.id;
+    console.log(props, id);
+    this.id =
+      typeof props.id === 'string'
+        ? new PartnerId(props.id)
+        : props.id ?? new PartnerId();
     this.name = props.name;
   }
 
   static create(command: { name: string }) {
-    return new Partner({ name: command.name });
+    return new Partner({
+      name: command.name,
+    });
   }
+
+  initEvent(command: InitEventCommand) {
+    return Event.create({
+      ...command,
+      partner_id: this.id,
+    });
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
+
   toJSON() {
     return {
       id: this.id,
